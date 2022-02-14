74aaedbbc244f3cbda45e418a11d489734e0194d
Younous2011 <younous.chinoune@gmail.com>
2022-02-14 05:56:50 -0500

    Write comments on my code

--- main.py
+++ main.py
@@ -1,24 +1,20 @@
-# Simple pygame program
+# Imports base class and colors
 from four_in_a_row.src.color import BLACK, RED, WHITE
 from four_in_a_row.src.position import Position
 from four_in_a_row.src.grill import Grill
 
-# Import and initialize the pygame library
-
+# Import pygame
 import pygame
 import pygame.gfxdraw
 
-
-
+# Init pygame
 pygame.init()
 
-from four_in_a_row.src.token import RED_TOKEN as red_token
-from four_in_a_row.src.square import BLUE_SQUARE as blue_square
+# Import after init
 from four_in_a_row.src.token import YELLOW_TOKEN as yellow_token
 from four_in_a_row.src.token_factory import TokenFactory
 
-# Set up the drawing window
-
+# Create all variables
 nb_lignes = 6
 nb_colonnes = 7
 l_box = 120
@@ -26,16 +22,18 @@ l_box = 120
 l_grill = l_box * 6 # 150 x 6 = 900
 L_grill = l_box * 7 # 200 x 7 = 1050
 
+# Width and height of my pygame-window
 screen = pygame.display.set_mode([L_grill, l_grill + l_box])
 
+# Create variables with value Class
 p = Position(0, l_box)
 g = Grill(l_box, nb_lignes, nb_colonnes, p)
 
 
 
 # l = Letter(Position(125, 125), "1", 200, BLACK)
+
 # Run until the user asks to quit
-list_position = [Position(0, 0)]*100
 token = TokenFactory().create(1, l_box // 2, RED, BLACK, "B")
 
 running = True
@@ -48,32 +46,30 @@ while running:
     for event in pygame.event.get():
         # handle MOUSEBUTTONUP
         if event.type == pygame.MOUSEBUTTONUP:
-            
             print(f"Vous êtes sur la colonne {Position(x, 0).get_column(l_box)}")
 
         if event.type == pygame.QUIT:
-
             running = False
 
 
     # Fill the background with white
-
     screen.fill(WHITE.get())
     
 
     x, y = pygame.mouse.get_pos()
     new_pos = Position(x, l_box // 2)
 
-    
-
+    # set_position of tokens
     token.set_position(new_pos)
-    token.blit_in(screen)
     yellow_token.set_position(Position(l_box // 2, L_grill - (l_box // 2)))
-    yellow_token.blit_in(screen)
 
+    # blit_in tokens
+    token.blit_in(screen)
+    yellow_token.blit_in(screen)
     g.blit_in(screen)
+
+    # Update display-pygame
     pygame.display.update()
 
 # Done! Time to quit.
-
 pygame.quit()
