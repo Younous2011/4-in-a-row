9df51462fb056568d413bab74a05fee8def88b31
Younous2011 <younous.chinoune@gmail.com>
2022-02-15 06:59:41 -0500

    feat : create token at the position click

--- main.py
+++ main.py
@@ -14,14 +14,15 @@ pygame.init()
 from four_in_a_row.src.token import YELLOW_TOKEN as yellow_token
 from four_in_a_row.src.token_factory import TokenFactory
 from four_in_a_row.src.grill_token import GrillToken
+from four_in_a_row.src.game import Game
 
 # Create all variables
 nb_lignes = 6
 nb_colonnes = 7
 l_box = 120
 
-l_grill = l_box * 6 # 150 x 6 = 900
-L_grill = l_box * 7 # 200 x 7 = 1050
+l_grill = l_box * 6 # 120 x 6 = 900
+L_grill = l_box * 7 # 120 x 7 = 840
 
 # Width and height of my pygame-window
 screen = pygame.display.set_mode([L_grill, l_grill + l_box])
@@ -31,25 +32,13 @@ p = Position(0, l_box)
 g = Grill(l_box, nb_lignes, nb_colonnes, p)
 gt = GrillToken(l_box, nb_lignes, nb_colonnes, p)
 
+game = Game(nb_lignes, nb_colonnes, None, None)
 
 
 # l = Letter(Position(125, 125), "1", 200, BLACK)
 
+token = TokenFactory().create(1, l_box // 2, RED, RED_DARK, "E")
 
-
-# Run until the user asks to quit
-token = TokenFactory().create(1, l_box // 2, RED, RED_DARK, "B")
-token2 = TokenFactory().create(1, l_box // 2, RED, RED_DARK, "U")
-token3 = TokenFactory().create(2, l_box // 2, YELLOW, YELLOW_DARK, "S")
-token4 = TokenFactory().create(1, l_box // 2, RED, RED_DARK, "7")
-token5 = TokenFactory().create(2, l_box // 2, YELLOW, YELLOW_DARK, "8")
-token6 = TokenFactory().create(1, l_box // 2, RED, RED_DARK, "B")
-token7 = TokenFactory().create(2, l_box // 2, YELLOW, YELLOW_DARK, "Y")
-
-gt.add_token(token3, 1, 1)
-gt.add_token(token2, 2, 0)
-gt.add_token(token5, 4,4)
-gt.add_token(token7, 5, 5)
 
 running = True
 
@@ -62,6 +51,7 @@ while running:
         # handle MOUSEBUTTONUP
         if event.type == pygame.MOUSEBUTTONUP:
             print(f"Vous êtes sur la colonne {Position(x, 0).get_column(l_box)}")
+            gt.add_token(token, 0, Position(x, 0).get_column(l_box))
 
         if event.type == pygame.QUIT:
             running = False
@@ -75,23 +65,9 @@ while running:
     new_pos = Position(x, l_box // 2)
 
     # set_position of tokens
-    token.set_position(new_pos)
-    token2.set_position(Position().get_position_token(0, 0, l_box, L_grill))
-    token3.set_position(Position().get_position_token(1, 0, l_box, L_grill))
-    token4.set_position(Position().get_position_token(2, 3, l_box, L_grill))
-    token5.set_position(Position().get_position_token(4, 6, l_box, L_grill))
-    token6.set_position(Position().get_position_token(5, 6, l_box, L_grill))
-    token7.set_position(Position().get_position_token(2, 2, l_box, L_grill))
-    yellow_token.set_position(Position().get_position_token(0, 6, l_box, L_grill))
-
     # blit_in tokens
-    """ token.blit_in(screen)
-    token2.blit_in(screen)
-    token3.blit_in(screen)
-    token4.blit_in(screen)
-    token5.blit_in(screen)
-    token6.blit_in(screen)
-    token7.blit_in(screen)"""
+    token.set_position(new_pos)
+    token.blit_in(screen)
     gt.blit_in(screen)
     g.blit_in(screen)
 
